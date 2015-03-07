f92147e8d7e8a9842d9154bd7a4737b896a44cdb
caixw <xnotepad@gmail.com>
2015-03-07 23:28:33 +0800

    添加doc.go文件

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright 2015 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// context提供了基于每个http请求期间的数据共享机制。
+package context
+
+const Version = "0.1.0.150307"
