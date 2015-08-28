171b788a58297c3b5d8a8a2b7fe4487fd5703bca
caixw <xnotepad@gmail.com>
2015-08-28 20:57:28 +0800

    删除版本号

--- context.go
+++ context.go
@@ -2,6 +2,20 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
+// context包提供了基于每个http请求期间的数据共享机制。
+//
+//  func h(w http.ResponseWriter, req *http.Request) {
+//      ctx := context.Get(req)
+//      ctx.Set("key", "val")
+//      // do somthing...
+//      var v string
+//      vi,found := ctx.Get("key")
+//      if found {
+//          v = vi.(string)
+//      }
+//
+//      v := ctx.MustString("key")
+//  }
 package context
 
 import (
